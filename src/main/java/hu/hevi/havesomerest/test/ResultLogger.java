8fde71ca5b7781ba19f85d62032f5b08f1432830
hevi <hever.istvan@gmail.com>
2016-07-10 12:35:50 +0100

    Added colorization to output

--- src/main/java/hu/hevi/havesomerest/test/ResultLogger.java
+++ src/main/java/hu/hevi/havesomerest/test/ResultLogger.java
@@ -5,27 +5,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 
-import java.text.MessageFormat;
-
 @Slf4j
 @Component
 public class ResultLogger {
 
     void logPassed(Test test, String finalEndPoint, ResponseEntity<String> resp) {
-        log.info(String.format("PASSED -> %-4s %3s /%-15s - %-35s",
+        String info = String.format((char)27 + "[32RmPASSED -> %-4s %3s /%-15s - %-35s",
                                       test.getMethod().toString().toUpperCase(),
                                       resp.getStatusCode().toString(),
                                       finalEndPoint,
-                                      test.getName()));
+                                      test.getName());
+        log.info(info);
     }
 
     void logFailed(Test test, String finalEndPoint, HttpClientErrorException e) {
-        String format = String.format("FAILED -> %-4s %3s /%-15s - %-35s -> %s",
+        String error = String.format((char)27 + "[31mFAILED -> %-4s %3s /%-15s - %-35s -> %s",
                                       test.getMethod().toString().toUpperCase(),
                                       test.getStatusCode(),
                                       finalEndPoint,
                                       test.getName(),
                                       e.getStatusCode() + " " + e.getStatusText());
-        log.error(format);
+        log.error(error);
     }
 }
