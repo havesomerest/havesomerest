ddb89c390eec1062e676a206b8d7b1871981f0cc
hevi <hever.istvan@gmail.com>
2016-07-31 21:14:14 +0100

    removing unnecessary stuff

--- src/main/java/hu/hevi/havesomerest/report/html/ReportGenerator.java
+++ src/main/java/hu/hevi/havesomerest/report/html/ReportGenerator.java
@@ -6,9 +6,6 @@ import hu.hevi.havesomerest.test.TestResult;
 import java.io.IOException;
 import java.util.Map;
 
-/**
- * Created by hevi on 30/07/16.
- */
 public interface ReportGenerator {
     void generateReport(Map<Test, TestResult> results) throws IOException;
 }
